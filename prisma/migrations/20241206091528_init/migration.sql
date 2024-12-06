1c93d7fe4c0f2ca72b87d61a4db4f7b479dbec2c
Delon00 <djplebob32@gmail.com>
2024-12-06 10:28:56 +0100

    modified:   .gitignore 	deleted:    prisma/migrations/20241126080356_v0_1/migration.sql 	deleted:    prisma/migrations/20241126100111_v0_1_2/migration.sql 	deleted:    prisma/migrations/20241205093203_v0_1_3/migration.sql 	renamed:    prisma/migrations/20241126073412_init/migration.sql -> prisma/migrations/20241206091528_init/migration.sql

--- prisma/migrations/20241206091528_init/migration.sql
+++ prisma/migrations/20241206091528_init/migration.sql
@@ -3,22 +3,26 @@ CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `nom` VARCHAR(191) NOT NULL,
+    `prenom` VARCHAR(191) NOT NULL,
     `bio` VARCHAR(191) NULL,
     `profile` BOOLEAN NOT NULL DEFAULT false,
 
     UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Post` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(191) NOT NULL,
-    `content` VARCHAR(191) NOT NULL,
-    `isPublic` BOOLEAN NOT NULL DEFAULT false,
+    `title` VARCHAR(191) NULL,
+    `content` VARCHAR(191) NULL,
     `userId` INTEGER NOT NULL,
     `mediaUrl` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
